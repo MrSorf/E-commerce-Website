cbf8560917b22a1aa959fd829404a254c3dc50f4
MrSorf <your_email>
2025-09-10 11:38:36 +0100

    started the e-commerce project; created my models

---
+++ products/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
+    stock_quantity = models.PositiveIntegerField()
+    image_url = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    added_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="products")
+
+    def __str__(self):
+        return self.name
